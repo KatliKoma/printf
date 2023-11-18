af4c72c020706dc304f9198318e8bdb6b851962a
FortuneMal <fmalaza512@gmail.com>
2023-11-17 16:14:19 -0800

    goodnight

--- _printf.c
+++ _printf.c
@@ -1,35 +1,35 @@
 #include "main.h"
 #include <stdarg.h>
+
 /**
- * _printf- Custom implementation of the printf function.
+ * _printf - Custom implementation of the printf function.
  * @format: A format string containing directives for printing.
  * Return: number of characters printed.
- * _printf("Complete the sentence: You %s nothing, Jon Snow.\n", (char *)0);
  */
+
 int _printf(const char *format, ...)
 {
-	int charPrinted = 0;
-
 	va_list args;
+	int charPrinted = 0;
 
 	va_start(args, format);
 
 	while (*format != '\0')
 	{
 		if (*format == '%' && *(format + 1) != '\0')
-		{
-			format++;
-			charPrinted = processSpecifier(format, args, charPrinted);
-			format++;
-		}
-		else
-		{
-			_putchar(*format);
-			charPrinted++;
-			format++;
-		}
-		if (*format == '\0')
-			break;
+	{
+		format++;
+	    charPrinted = processSpecifier(format, args, charPrinted);
+	    format++;
+	}
+	else
+	{
+		_putchar(*format);
+	    charPrinted++;
+	    format++;
+	}
+	if (*format == '\0')
+		break;
 	}
 	va_end(args);
 	return (charPrinted);
